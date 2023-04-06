84cfdeb7cd448b4c349a54a957736900b8e8629e
vlad <vladislavludinov3006@gmail.com>
2023-04-06 19:02:55 +0300

    added work function with some elements

--- Task38.py
+++ Task38.py
@@ -4,18 +4,22 @@ def read_directory():
     phone_directory = open("phone_directory.txt", "r")
     directory = phone_directory.readlines()
     if read_input == 1:
+        print()
         for line in directory:
             print(line, end = "")
         print()
     elif read_input == 2:
         print("Введите имя, номер телефона или коментарий интересующий вас:")
         clarification = input()
-        find_line = find_element_directory(directory, clarification)
-        if find_line == None:
+        print()
+        find_lines = find_all_element_directory(directory, clarification)
+        if find_lines == []:
             print("Такого в справочнике нет")
-            print()
         else:
-            print(find_line)
+            for line in find_lines:
+                print(line, end = "")
+            print()
+    print()
     phone_directory.close()
 
 def append_new_in_directory():
@@ -26,38 +30,60 @@ def append_new_in_directory():
     phone_directory.writelines(f"\n{name} - {phone_number} - {comment}")
     phone_directory.close()
 
-def find_element_directory(data, element):
+def find_all_element_directory(data, element):
+    lines = []
     for line in data:
         words = line.split(" - ")
         for parameter in words:
             if element == parameter:
-                return line
+                lines.append(line)
+    return lines
 
 def change_directory():
     phone_directory1 = open("phone_directory.txt", "r")
     print("Введите элемент который хотите изменить:")
     replaced_element = input()
     directory = phone_directory1.readlines()
-    replaced_line = find_element_directory(directory, replaced_element)
-    if replaced_line == None:
+    replaced_lines = find_all_element_directory(directory, replaced_element)
+    if replaced_lines == []:
+        print()
         print("Такого элемента нет")
     else:
         print("Введите элемент на который вы хотите заменить:")
         replacing_element = input()
-        split_replaced_line = replaced_line.split(" - ")
-        split_replaced_line.insert(split_replaced_line.index(replaced_element), replacing_element)
-        split_replaced_line.pop(split_replaced_line.index(replaced_element))
-        replacing_line = " - ".join(split_replaced_line)
-        directory.insert(directory.index(replaced_line), replacing_line)
-        directory.pop(directory.index(replaced_line))
+        for line in replaced_lines:
+            split_replaced_line = line.split(" - ")
+            split_replaced_line.insert(split_replaced_line.index(replaced_element), replacing_element)
+            split_replaced_line.pop(split_replaced_line.index(replaced_element))
+            replacing_line = " - ".join(split_replaced_line)
+            directory.insert(directory.index(line), replacing_line)
+            directory.pop(directory.index(line))
+        phone_directory2 = open("phone_directory.txt", "w")
+        for line in directory:
+            phone_directory2.writelines(line)
+        phone_directory2.close()
+        print()
+        print("Готово")
+    phone_directory1.close()
+    print()
+        
+def delete_element_directory():
+    phone_directory1 = open("phone_directory.txt", "r")
+    print("Введите элемент, записи с которым вы хотите удалить:")
+    remote_element = input()
+    directory = phone_directory1.readlines()
+    remote_lines = find_all_element_directory(directory, remote_element)
+    if remote_element == []:
+        print("Такого элемента в справочнике нет")
+    else:
+        for line in remote_lines:
+            directory.pop(directory.index(line))
         phone_directory2 = open("phone_directory.txt", "w")
         for line in directory:
             phone_directory2.writelines(line)
         phone_directory2.close()
     phone_directory1.close()
     print()
-        
-
 
 menu_user_input = None
 
@@ -70,6 +96,8 @@ while menu_user_input != 9:
         append_new_in_directory()
     elif menu_user_input == 3:
         change_directory()
+    elif menu_user_input == 4:
+        delete_element_directory()
     
 
 
